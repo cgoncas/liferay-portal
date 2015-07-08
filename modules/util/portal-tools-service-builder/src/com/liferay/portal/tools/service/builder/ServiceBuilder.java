621a3b06725da1b719ed6a4b7649c3bec80be218
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-07-08 11:53:19 +0200

    LPS-55475 Support adding companyId column for mapping tables in ServiceBuilder

--- modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -119,6 +119,7 @@ import org.dom4j.io.SAXReader;
  * @author Cody Hoag
  * @author James Hinkey
  * @author Hugo Huijser
+ * @author Manuel de la Peña
  */
 public class ServiceBuilder {
 
@@ -4669,7 +4670,8 @@ public class ServiceBuilder {
 				Validator.isNotNull(mappingTable)) {
 
 				EntityMapping entityMapping = new EntityMapping(
-					mappingTable, ejbName, collectionEntity);
+					mappingTable, getEntity("com.liferay.portal.Company"),
+					ejbName, collectionEntity);
 
 				if (!_entityMappings.containsKey(mappingTable)) {
 					_entityMappings.put(mappingTable, entityMapping);
