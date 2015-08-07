8b88622181be4aa347a54d261a447b5c00b01207
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-08-07 09:30:52 +0200

    LPS-55475 Upgrade process to add the column companyId to the wiki tables and fill it with 0

---
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/upgrade/v1_0_0/UpgradeSharding.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ * @author Cristina González
+ */
+public class UpgradeSharding extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("alter table WikiPageResource add companyId LONG;");
+
+		_updateCompanyId("WikiPageResource");
+	}
+
+	private void _updateCompanyId(String tableName) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"update " + tableName + " set companyId = ?");
+
+			ps.setLong(1, 0);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+	}
+
+}
