9848a76c83161dea21161bd2320d1d58ffa8962e
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-08-17 08:57:08 +0200

    LPS-55475 Upgrade process to add the column companyId to the journal tables and fill it with 0

--- modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/v1_0_0/dependencies/update.sql
+++ modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/v1_0_0/dependencies/update.sql
@@ -1,9 +1,12 @@
+create index IX_CC7576C7 on JournalArticleResource (uuid_, companyId);
+
 alter table JournalFolder add restrictionType INTEGER;
 
 create table JournalFolders_DDMStructures (
+	companyId LONG not null,
 	structureId LONG not null,
 	folderId LONG not null,
-	primary key (structureId, folderId)
+	primary key (structureId, folderId, companyId)
 );
 
 COMMIT_TRANSACTION;
