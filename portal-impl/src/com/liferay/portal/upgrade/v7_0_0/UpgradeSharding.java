edb6ceab08ab6e6ff7196aed62548812bd3ca0d1
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-10-27 17:30:26 +0100

    LPS-55475 Let's put the instruction in a single place

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeSharding.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeSharding.java
@@ -33,9 +33,12 @@ import com.liferay.portal.upgrade.v7_0_0.util.ResourceActionTable;
 import com.liferay.portal.upgrade.v7_0_0.util.VirtualHostTable;
 import com.liferay.portal.util.PropsUtil;
 
+import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,151 +51,49 @@ import javax.sql.DataSource;
  */
 public class UpgradeSharding extends UpgradeProcess {
 
+	protected void addCompanyIdColumn(String tableName)
+		throws IOException, SQLException {
+
+		runSQL("alter table " + tableName + " add companyId LONG default 0");
+	}
+
 	protected void addCompanyIdToTables() throws Exception {
-		runSQL("alter table AnnouncementsFlag add companyId LONG;");
-
-		initCompanyId("AnnouncementsFlag");
-
-		runSQL("alter table AssetEntries_AssetCategories add companyId LONG;");
-
-		initCompanyId("AssetEntries_AssetCategories");
-
-		runSQL("alter table AssetEntries_AssetTags add companyId LONG;");
-
-		initCompanyId("AssetEntries_AssetTags");
-
-		runSQL("alter table AssetTagStats add companyId LONG;");
-
-		initCompanyId("AssetTagStats");
-
-		runSQL("alter table BrowserTracker add companyId LONG;");
-
-		initCompanyId("BrowserTracker");
-
-		runSQL("alter table DLFileEntryMetadata add companyId LONG;");
-
-		initCompanyId("DLFileEntryMetadata");
-
-		runSQL("alter table DLFileEntryTypes_DLFolders add companyId LONG;");
-
-		initCompanyId("DLFileEntryTypes_DLFolders");
-
-		runSQL("alter table DLSyncEvent add companyId LONG;");
-
-		initCompanyId("DLSyncEvent");
-
-		runSQL("alter table Groups_Orgs add companyId LONG;");
-
-		initCompanyId("Groups_Orgs");
-
-		runSQL("alter table Groups_Roles add companyId LONG;");
-
-		initCompanyId("Groups_Roles");
-
-		runSQL("alter table Groups_UserGroups add companyId LONG;");
-
-		initCompanyId("Groups_UserGroups");
-
-		runSQL("alter table Image add companyId LONG;");
-
-		initCompanyId("Image");
-
-		runSQL("alter table Marketplace_Module add companyId LONG;");
-
-		initCompanyId("Marketplace_Module");
-
-		runSQL("alter table MBStatsUser add companyId LONG;");
-
-		initCompanyId("MBStatsUser");
-
-		runSQL("alter table OrgGroupRole add companyId LONG;");
-
-		initCompanyId("OrgGroupRole");
-
-		runSQL("alter table OrgLabor add companyId LONG;");
-
-		initCompanyId("OrgLabor");
-
-		runSQL("alter table PasswordPolicyRel add companyId LONG;");
-
-		initCompanyId("PasswordPolicyRel");
-
-		runSQL("alter table PasswordTracker add companyId LONG;");
-
-		initCompanyId("PasswordTracker");
-
-		runSQL("alter table PortletPreferences add companyId LONG;");
-
-		initCompanyId("PortletPreferences");
-
-		runSQL("alter table RatingsStats add companyId LONG;");
-
-		initCompanyId("RatingsStats");
-
-		runSQL("alter table ResourceBlockPermission add companyId LONG;");
-
-		initCompanyId("ResourceBlockPermission");
-
-		runSQL(
-			"alter table SCFrameworkVersi_SCProductVers add companyId LONG;");
-
-		initCompanyId("SCFrameworkVersi_SCProductVers");
-
-		runSQL("alter table SCLicense add companyId LONG;");
-
-		initCompanyId("SCLicense");
-
-		runSQL("alter table SCLicenses_SCProductEntries add companyId LONG;");
-
-		initCompanyId("SCLicenses_SCProductEntries");
-
-		runSQL("alter table ServiceComponent add companyId LONG;");
-
-		initCompanyId("ServiceComponent");
-
-		runSQL("alter table TrashVersion add companyId LONG;");
-
-		initCompanyId("TrashVersion");
-
-		runSQL("alter table UserGroupGroupRole add companyId LONG;");
-
-		initCompanyId("UserGroupGroupRole");
-
-		runSQL("alter table UserGroupRole add companyId LONG;");
-
-		initCompanyId("UserGroupRole");
-
-		runSQL("alter table UserGroups_Teams add companyId LONG;");
-
-		initCompanyId("UserGroups_Teams");
-
-		runSQL("alter table UserIdMapper add companyId LONG;");
-
-		initCompanyId("UserIdMapper");
-
-		runSQL("alter table Users_Groups add companyId LONG;");
-
-		initCompanyId("Users_Groups");
-
-		runSQL("alter table Users_Orgs add companyId LONG;");
-
-		initCompanyId("Users_Orgs");
-
-		runSQL("alter table Users_Roles add companyId LONG;");
-
-		initCompanyId("Users_Roles");
-
-		runSQL("alter table Users_Teams add companyId LONG;");
-
-		initCompanyId("Users_Teams");
-
-		runSQL("alter table Users_UserGroups add companyId LONG;");
-
-		initCompanyId("Users_UserGroups");
-
-		runSQL("alter table UserTrackerPath add companyId LONG;");
-
-		initCompanyId("UserTrackerPath");
+		addCompanyIdColumn("AnnouncementsFlag");
+		addCompanyIdColumn("AssetEntries_AssetCategories");
+		addCompanyIdColumn("AssetEntries_AssetTags");
+		addCompanyIdColumn("AssetTagStats");
+		addCompanyIdColumn("BrowserTracker");
+		addCompanyIdColumn("DLFileEntryMetadata");
+		addCompanyIdColumn("DLFileEntryTypes_DLFolders");
+		addCompanyIdColumn("DLSyncEvent");
+		addCompanyIdColumn("Groups_Orgs");
+		addCompanyIdColumn("Groups_Roles");
+		addCompanyIdColumn("Groups_UserGroups");
+		addCompanyIdColumn("Image");
+		addCompanyIdColumn("Marketplace_Module");
+		addCompanyIdColumn("MBStatsUser");
+		addCompanyIdColumn("OrgGroupRole");
+		addCompanyIdColumn("OrgLabor");
+		addCompanyIdColumn("PasswordPolicyRel");
+		addCompanyIdColumn("PasswordTracker");
+		addCompanyIdColumn("PortletPreferences");
+		addCompanyIdColumn("RatingsStats");
+		addCompanyIdColumn("ResourceBlockPermission");
+		addCompanyIdColumn("SCFrameworkVersi_SCProductVers");
+		addCompanyIdColumn("SCLicense");
+		addCompanyIdColumn("SCLicenses_SCProductEntries");
+		addCompanyIdColumn("ServiceComponent");
+		addCompanyIdColumn("TrashVersion");
+		addCompanyIdColumn("UserGroupGroupRole");
+		addCompanyIdColumn("UserGroupRole");
+		addCompanyIdColumn("UserGroups_Teams");
+		addCompanyIdColumn("UserIdMapper");
+		addCompanyIdColumn("Users_Groups");
+		addCompanyIdColumn("Users_Orgs");
+		addCompanyIdColumn("Users_Roles");
+		addCompanyIdColumn("Users_Teams");
+		addCompanyIdColumn("Users_UserGroups");
+		addCompanyIdColumn("UserTrackerPath");
 	}
 
 	protected void copyControlTable(
@@ -318,25 +219,6 @@ public class UpgradeSharding extends UpgradeProcess {
 		}
 
 		return shardNames;
-	}
-
-	protected void initCompanyId(String tableName) throws Exception {
-		Connection con = null;
-		PreparedStatement ps = null;
-
-		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
-				"update " + tableName + " set companyId = ?");
-
-			ps.setLong(1, 0);
-
-			ps.executeUpdate();
-		}
-		finally {
-			DataAccess.cleanUp(con, ps);
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
