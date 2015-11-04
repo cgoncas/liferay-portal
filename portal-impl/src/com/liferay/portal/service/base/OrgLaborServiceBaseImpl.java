0d7356748b12de72fc4b001726e0c43fc84b0a01
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-11-04 12:55:07 +0100

    LPS-55475 AUTOGENERATED

--- portal-impl/src/com/liferay/portal/service/base/OrgLaborServiceBaseImpl.java
+++ portal-impl/src/com/liferay/portal/service/base/OrgLaborServiceBaseImpl.java
@@ -26,6 +26,8 @@ import com.liferay.portal.service.BaseServiceImpl;
 import com.liferay.portal.service.OrgLaborService;
 import com.liferay.portal.service.persistence.ListTypePersistence;
 import com.liferay.portal.service.persistence.OrgLaborPersistence;
+import com.liferay.portal.service.persistence.OrganizationFinder;
+import com.liferay.portal.service.persistence.OrganizationPersistence;
 import com.liferay.portal.util.PortalUtil;
 
 import javax.sql.DataSource;
@@ -180,6 +182,81 @@ public abstract class OrgLaborServiceBaseImpl extends BaseServiceImpl
 		this.listTypePersistence = listTypePersistence;
 	}
 
+	/**
+	 * Returns the organization local service.
+	 *
+	 * @return the organization local service
+	 */
+	public com.liferay.portal.service.OrganizationLocalService getOrganizationLocalService() {
+		return organizationLocalService;
+	}
+
+	/**
+	 * Sets the organization local service.
+	 *
+	 * @param organizationLocalService the organization local service
+	 */
+	public void setOrganizationLocalService(
+		com.liferay.portal.service.OrganizationLocalService organizationLocalService) {
+		this.organizationLocalService = organizationLocalService;
+	}
+
+	/**
+	 * Returns the organization remote service.
+	 *
+	 * @return the organization remote service
+	 */
+	public com.liferay.portal.service.OrganizationService getOrganizationService() {
+		return organizationService;
+	}
+
+	/**
+	 * Sets the organization remote service.
+	 *
+	 * @param organizationService the organization remote service
+	 */
+	public void setOrganizationService(
+		com.liferay.portal.service.OrganizationService organizationService) {
+		this.organizationService = organizationService;
+	}
+
+	/**
+	 * Returns the organization persistence.
+	 *
+	 * @return the organization persistence
+	 */
+	public OrganizationPersistence getOrganizationPersistence() {
+		return organizationPersistence;
+	}
+
+	/**
+	 * Sets the organization persistence.
+	 *
+	 * @param organizationPersistence the organization persistence
+	 */
+	public void setOrganizationPersistence(
+		OrganizationPersistence organizationPersistence) {
+		this.organizationPersistence = organizationPersistence;
+	}
+
+	/**
+	 * Returns the organization finder.
+	 *
+	 * @return the organization finder
+	 */
+	public OrganizationFinder getOrganizationFinder() {
+		return organizationFinder;
+	}
+
+	/**
+	 * Sets the organization finder.
+	 *
+	 * @param organizationFinder the organization finder
+	 */
+	public void setOrganizationFinder(OrganizationFinder organizationFinder) {
+		this.organizationFinder = organizationFinder;
+	}
+
 	public void afterPropertiesSet() {
 	}
 
@@ -242,4 +319,12 @@ public abstract class OrgLaborServiceBaseImpl extends BaseServiceImpl
 	protected com.liferay.portal.service.ListTypeService listTypeService;
 	@BeanReference(type = ListTypePersistence.class)
 	protected ListTypePersistence listTypePersistence;
+	@BeanReference(type = com.liferay.portal.service.OrganizationLocalService.class)
+	protected com.liferay.portal.service.OrganizationLocalService organizationLocalService;
+	@BeanReference(type = com.liferay.portal.service.OrganizationService.class)
+	protected com.liferay.portal.service.OrganizationService organizationService;
+	@BeanReference(type = OrganizationPersistence.class)
+	protected OrganizationPersistence organizationPersistence;
+	@BeanReference(type = OrganizationFinder.class)
+	protected OrganizationFinder organizationFinder;
 }
