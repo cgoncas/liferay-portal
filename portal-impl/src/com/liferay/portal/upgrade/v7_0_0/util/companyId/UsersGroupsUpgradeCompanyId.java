013120eecf12a91c5055ff081624b1f297679a46
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-11-04 11:21:46 +0100

    LPS-55475 Use the interface UpgradeCompanyIdInTable in portal-impl

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/util/companyId/UsersGroupsUpgradeCompanyId.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_0.util.companyId;
+
+import com.liferay.portal.kernel.upgrade.util.UpgradeCompanyIdInTable;
+
+/**
+ * @author Cristina González
+ */
+public class UsersGroupsUpgradeCompanyId implements UpgradeCompanyIdInTable {
+
+	@Override
+	public String getTableName() {
+		return "Users_Groups";
+	}
+
+	@Override
+	public void upgradeProcess() {
+	}
+
+}
