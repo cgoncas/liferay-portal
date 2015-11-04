c8b9e3db9ec1d383d09c79b8ed7d556982f63cca
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-11-04 13:36:08 +0100

    LPS-55475 AUTOGENERATED

--- portal-service/src/com/liferay/portlet/trash/service/TrashVersionLocalServiceUtil.java
+++ portal-service/src/com/liferay/portlet/trash/service/TrashVersionLocalServiceUtil.java
@@ -43,7 +43,8 @@ public class TrashVersionLocalServiceUtil {
 	public static com.liferay.portlet.trash.model.TrashVersion addTrashVersion(
 		long trashEntryId, java.lang.String className, long classPK,
 		int status,
-		com.liferay.portal.kernel.util.UnicodeProperties typeSettingsProperties) {
+		com.liferay.portal.kernel.util.UnicodeProperties typeSettingsProperties)
+		throws com.liferay.portal.kernel.exception.PortalException {
 		return getService()
 				   .addTrashVersion(trashEntryId, className, classPK, status,
 			typeSettingsProperties);
