dc97f8ca051f5d1a9e10392fccb7145e20b011e0
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-11-04 16:26:13 +0100

    LPS-55475 Use the interface UpgradeCompanyIdInTable in wiki

--- modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/upgrade/v1_0_0/util/companyId/WikiPageResourceUpgradeCompanyId.java
+++ modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/upgrade/v1_0_0/util/companyId/WikiPageResourceUpgradeCompanyId.java
@@ -12,22 +12,23 @@
  * details.
  */
 
-package com.liferay.wiki.upgrade.v1_0_0;
+package com.liferay.wiki.upgrade.v1_0_0.util.companyId;
 
 import com.liferay.portal.kernel.upgrade.util.UpgradeCompanyIdInTable;
-import com.liferay.wiki.upgrade.v1_0_0.util.companyId.WikiPageResourceUpgradeCompanyId;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Cristina González
  */
-public class UpgradeCompanyId
-	extends com.liferay.portal.upgrade.util.UpgradeCompanyId {
+public class WikiPageResourceUpgradeCompanyId
+	implements UpgradeCompanyIdInTable {
 
 	@Override
-	protected UpgradeCompanyIdInTable[] getUpgradeCompanyIdInTable() {
-		return new UpgradeCompanyIdInTable[] {
-			new WikiPageResourceUpgradeCompanyId()
-		};
+	public String getTableName() {
+		return "WikiPageResource";
+	}
+
+	@Override
+	public void upgradeProcess() {
 	}
 
 }
