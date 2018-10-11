5e8f8089a9fb1b2ad3f7dacd09b0636e1dd7f86a
rubenpulido <ruben.pulido@liferay.com>
2018-10-11 14:35:38 +0200

    LPS-86324 testDefaultTitleIsDisplayedWhenLocaleESRequestedIsNotAvailable

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -120,6 +120,43 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testDefaultTitleIsDisplayedWhenLocaleESRequestedIsNotAvailable()
+		throws Exception {
+
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headersMap = _getDefaultHeadersMap();
+
+		headersMap.put("Accept-Language", "es-ES");
+
+		List<String> titles = JsonPath.read(
+			_toStringAsGuest(hrefs.get(0), headersMap),
+			"$._embedded.StructuredContent[*].title");
+
+		Assert.assertTrue(
+			titles.contains(
+				StructuredContentApioTestBundleActivator.
+					TITLE1_LOCALE_DEFAULT));
+
+		Assert.assertTrue(
+			titles.contains(
+				StructuredContentApioTestBundleActivator.
+					TITLE2_LOCALE_DEFAULT));
+
+		Assert.assertTrue(
+			titles.contains(
+				StructuredContentApioTestBundleActivator.
+					TITLE3_LOCALE_DEFAULT));
+	}
+
+	@Test
 	public void testDefaultTitleIsDisplayedWhenLocaleFRRequestedIsNotAvailable()
 		throws Exception {
 
