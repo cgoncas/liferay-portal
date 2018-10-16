df8f9e945b14e0724efb56e158eebf8ec03c85c1
rubenpulido <ruben.pulido@liferay.com>
2018-10-17 00:38:04 +0200

    LPS-86381 testDefaultStructuredFieldValueIsDisplayedWhenAcceptLanguageIsSpecifiedAndDoesNotMatch

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -88,6 +88,39 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testDefaultStructuredFieldValueIsDisplayedWhenAcceptLanguageIsSpecifiedAndDoesNotMatch()
+		throws Exception {
+
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headers = _getHeaders();
+
+		headers.put("Accept-Language", "de-DE");
+
+		List<String> values = JsonPath.read(
+			_toStringAsGuest(hrefs.get(0), headers),
+			"$._embedded.StructuredContent[*]._embedded.values._embedded[*]." +
+				"value");
+
+		Assert.assertTrue(
+			values.contains(
+				StructuredContentApioTestBundleActivator.
+					TEXT_FIELD_VALUE.getString(LocaleUtil.US)));
+
+		Assert.assertTrue(
+			values.contains(
+				StructuredContentApioTestBundleActivator.
+					NESTED_TEXT_FIELD_VALUE.getString(LocaleUtil.US)));
+	}
+
+	@Test
 	public void testDefaultTitleIsDisplayedWhenAcceptLanguageIsNotSpecified()
 		throws Exception {
 
