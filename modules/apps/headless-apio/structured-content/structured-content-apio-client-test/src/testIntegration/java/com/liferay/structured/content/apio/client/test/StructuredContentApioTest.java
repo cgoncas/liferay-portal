a793ad759be4dc709e4c91cf30743116bf1d52a0
rubenpulido <ruben.pulido@liferay.com>
2018-10-23 13:03:02 +0200

    LPS-85301 testGeolocationDataTypeIsDisplayed

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -316,6 +316,34 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testGeolocationDataTypeIsDisplayed() throws Exception {
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headers = _getHeaders();
+
+		headers.put("Accept-Language", "en-US");
+
+		List<String> dataTypes = JsonPath.read(
+			_toStringAsGuest(
+				_getURLWithFilterByTitle(
+					hrefs.get(0),
+					StructuredContentApioTestBundleActivator.
+						TITLE_GEOLOCATION_FIELD_LOCALE_US),
+				headers),
+			"$._embedded.StructuredContent[*]._embedded.values._embedded[*]." +
+				"dataType");
+
+		Assert.assertTrue(dataTypes.contains("geolocation"));
+	}
+
+	@Test
 	public void testGuestUserSeesRightStructuredContents() throws Exception {
 		List<String> hrefs = JsonPath.read(
 			_toStringAsAdmin(
