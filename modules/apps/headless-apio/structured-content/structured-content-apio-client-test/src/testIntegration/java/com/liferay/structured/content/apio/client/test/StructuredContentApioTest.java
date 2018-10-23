e4255c2fe2e0708562351a417f9ce4e3f57d6b85
rubenpulido <ruben.pulido@liferay.com>
2018-10-23 10:07:24 +0200

    LPS-85301 testBooleanFieldDataTypeIsDisplayed

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -93,6 +93,34 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testBooleanFieldDataTypeIsDisplayed() throws Exception {
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headers = _getHeaders();
+
+		headers.put("Accept-Language", "en-US");
+
+		List<String> dataTypes = JsonPath.read(
+			_toStringAsGuest(
+				_getURLWithFilterByTitle(
+					hrefs.get(0),
+					StructuredContentApioTestBundleActivator.
+						TITLE_BOOLEAN_FIELD_LOCALE_US),
+				headers),
+			"$._embedded.StructuredContent[*]._embedded.values._embedded[*]." +
+				"dataType");
+
+		Assert.assertTrue(dataTypes.contains("boolean"));
+	}
+
+	@Test
 	public void testDefaultStructuredFieldLabelIsDisplayedWhenAcceptLanguageIsSpecifiedAndDoesNotMatch()
 		throws Exception {
 
