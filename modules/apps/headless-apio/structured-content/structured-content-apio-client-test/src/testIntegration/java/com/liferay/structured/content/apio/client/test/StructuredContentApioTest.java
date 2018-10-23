8b4b11994722f7a51319db783b6616b884c4b3c6
rubenpulido <ruben.pulido@liferay.com>
2018-10-23 12:39:54 +0200

    LPS-85301 testDocumentsAndMediaFieldDataTypeIsDisplayed

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -286,6 +286,36 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testDocumentsAndMediaFieldDataTypeIsDisplayed()
+		throws Exception {
+
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headers = _getHeaders();
+
+		headers.put("Accept-Language", "en-US");
+
+		List<String> dataTypes = JsonPath.read(
+			_toStringAsGuest(
+				_getURLWithFilterByTitle(
+					hrefs.get(0),
+					StructuredContentApioTestBundleActivator.
+						TITLE_DOCUMENTS_AND_MEDIA_FIELD_LOCALE_US),
+				headers),
+			"$._embedded.StructuredContent[*]._embedded.values._embedded[*]." +
+				"dataType");
+
+		Assert.assertTrue(dataTypes.contains("document-library"));
+	}
+
+	@Test
 	public void testGuestUserSeesRightStructuredContents() throws Exception {
 		List<String> hrefs = JsonPath.read(
 			_toStringAsAdmin(
