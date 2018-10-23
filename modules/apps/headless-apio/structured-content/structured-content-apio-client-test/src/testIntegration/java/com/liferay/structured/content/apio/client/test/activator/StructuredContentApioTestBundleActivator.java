32141190e9bdbd12c657f27da11f39f6ee9b41cb
rubenpulido <ruben.pulido@liferay.com>
2018-10-23 12:35:58 +0200

    LPS-85301 Add journal article with documents-and-media field

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/activator/StructuredContentApioTestBundleActivator.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/activator/StructuredContentApioTestBundleActivator.java
@@ -107,6 +107,10 @@ public class StructuredContentApioTestBundleActivator
 		StructuredContentApioTestBundleActivator.class.getSimpleName() +
 			"TitleDecimalField_us";
 
+	public static final String TITLE_DOCUMENTS_AND_MEDIA_FIELD_LOCALE_US =
+		StructuredContentApioTestBundleActivator.class.getSimpleName() +
+			"TitleDocumentsAndMediaField_us";
+
 	public static final String TITLE_NO_GUEST_NO_GROUP =
 		StructuredContentApioTestBundleActivator.class.getSimpleName() +
 			"NoGuestNoGroupTitle";
@@ -316,6 +320,10 @@ public class StructuredContentApioTestBundleActivator
 		_addJournalArticle(
 			user, group, "test-journal-decimal-field",
 			TITLE_DECIMAL_FIELD_LOCALE_US);
+
+		_addJournalArticle(
+			user, group, "test-journal-documents-and-media-field",
+			TITLE_DOCUMENTS_AND_MEDIA_FIELD_LOCALE_US);
 	}
 
 	private void _prepareDataForLocalizationTests(User user, Group group)
