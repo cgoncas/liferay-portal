f441d985aa24c97bec5934a861b445b290e9acc8
rubenpulido <ruben.pulido@liferay.com>
2018-10-24 15:25:24 +0200

    LPS-85301 TestDecimalFieldDataTypeIsDisplayed

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -180,6 +180,35 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
+	public void testDecimalFieldDataTypeIsDisplayed() throws Exception {
+		List<String> hrefs = JsonPath.read(
+			_toStringAsAdmin(
+				JsonPath.read(
+					_toStringAsAdmin(_rootEndpointURL.toExternalForm()),
+					"$._links.content-space.href")),
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
+
+		Map<String, String> headers = _getHeaders();
+
+		headers.put("Accept-Language", "en-US");
+
+		List<String> dataTypes = JsonPath.read(
+			_toStringAsGuest(
+				_getURLWithFilterByTitle(
+					hrefs.get(0),
+					StructuredContentApioTestBundleActivator.
+						TITLE_2_LOCALE_DEFAULT),
+				headers),
+			"$._embedded.StructuredContent[*]._embedded.values._embedded" +
+				"[?(@.name=='MyDecimal')].dataType");
+
+		Assert.assertEquals(dataTypes.toString(), 1, dataTypes.size());
+		Assert.assertTrue(dataTypes.contains("double"));
+	}
+
+	@Test
 	public void testDefaultStructuredFieldLabelIsDisplayedWhenAcceptLanguageIsSpecifiedAndDoesNotMatch()
 		throws Exception {
 
