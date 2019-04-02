8ef95a9accf9b5d139fdaed9a8311fea500acd59
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-04-02 18:01:43 +0200

    LPS-91295 New api method for supporting localized texts.

--- modules/apps/asset/asset-auto-tagger-google-cloud-natural-language-api/src/main/java/com/liferay/asset/auto/tagger/google/cloud/natural/language/api/GCloudNaturalLanguageDocumentAssetAutoTagger.java
+++ modules/apps/asset/asset-auto-tagger-google-cloud-natural-language-api/src/main/java/com/liferay/asset/auto/tagger/google/cloud/natural/language/api/GCloudNaturalLanguageDocumentAssetAutoTagger.java
@@ -17,6 +17,7 @@ package com.liferay.asset.auto.tagger.google.cloud.natural.language.api;
 import com.liferay.asset.auto.tagger.google.cloud.natural.language.api.configuration.GCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration;
 
 import java.util.Collection;
+import java.util.Locale;
 
 /**
  * Models a Google Cloud Natural Language Document Asset Auto Tagger.
@@ -25,6 +26,23 @@ import java.util.Collection;
  * @review
  */
 public interface GCloudNaturalLanguageDocumentAssetAutoTagger {
+
+	/**
+	 * Returns a list of tag names from Google Cloud Natural Language
+	 * Classification API.
+	 *
+	 * @param gCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration the configuration.
+	 * @param content the text to be tagged.
+	 * @param locale the locale of the text.
+	 * @param mimeType the text mimeType.
+	 * @return a list of tag names.
+	 * @review
+	 */
+	public Collection<String> getTagNames(
+			GCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration
+				gCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration,
+			String content, Locale locale, String mimeType)
+		throws Exception;
 
 	/**
 	 * Returns a list of tag names from Google Cloud Natural Language
