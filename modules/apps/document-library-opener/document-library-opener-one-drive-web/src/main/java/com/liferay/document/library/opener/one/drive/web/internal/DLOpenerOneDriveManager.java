8fe2a2614d9e51d8f5d00e301fa169f0f949b6c1
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-07-09 10:48:20 +0200

    LPS-83411 Add toolbar contributor.

---
+++ modules/apps/document-library-opener/document-library-opener-one-drive-web/src/main/java/com/liferay/document/library/opener/one/drive/web/internal/DLOpenerOneDriveManager.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.opener.one.drive.web.internal;
+
+import com.liferay.document.library.opener.one.drive.web.internal.portlet.configuration.DLOneDriveCompanyConfiguration;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.util.Validator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = DLOpenerOneDriveManager.class)
+public class DLOpenerOneDriveManager {
+
+	public boolean isConfigured(long companyId) throws ConfigurationException {
+		DLOneDriveCompanyConfiguration dlOneDriveCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				DLOneDriveCompanyConfiguration.class, companyId);
+
+		if (Validator.isNotNull(dlOneDriveCompanyConfiguration.clientId()) &&
+			Validator.isNotNull(
+				dlOneDriveCompanyConfiguration.clientSecret()) &&
+			Validator.isNotNull(dlOneDriveCompanyConfiguration.tenant())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
