b4c6b26cd0b10bc603e2d7589eef14243f9c5c7c
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-07-18 16:19:25 +0200

    LPS-117280 Add to GetContentDashboardItemInfoMVCResourceCommand

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/portlet/action/GetContentDashboardItemInfoMVCResourceCommand.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/portlet/action/GetContentDashboardItemInfoMVCResourceCommand.java
@@ -132,6 +132,10 @@ public class GetContentDashboardItemInfoMVCResourceCommand
 				).put(
 					"userName", contentDashboardItem.getUserName()
 				).put(
+					"userPortraitURL",
+					contentDashboardItem.getUserPortraitURL(
+						_portal.getHttpServletRequest(resourceRequest))
+				).put(
 					"versions",
 					_getVersionsJSONArray(contentDashboardItem, locale)
 				).put(
