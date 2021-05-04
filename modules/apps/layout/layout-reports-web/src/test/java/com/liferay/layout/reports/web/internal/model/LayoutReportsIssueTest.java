2a13767dd73e4189f1f74c18c362f6f627a4638b
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-05-04 09:33:21 +0200

    LPS-119066 Test it

--- modules/apps/layout/layout-reports-web/src/test/java/com/liferay/layout/reports/web/internal/model/LayoutReportsIssueTest.java
+++ modules/apps/layout/layout-reports-web/src/test/java/com/liferay/layout/reports/web/internal/model/LayoutReportsIssueTest.java
@@ -15,10 +15,15 @@
 package com.liferay.layout.reports.web.internal.model;
 
 import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -37,7 +42,7 @@ public class LayoutReportsIssueTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
 
 		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
@@ -46,9 +51,13 @@ public class LayoutReportsIssueTest {
 	@Test
 	public void testEquals() {
 		LayoutReportsIssue layoutReportsIssue1 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 		LayoutReportsIssue layoutReportsIssue2 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 
 		Assert.assertTrue(layoutReportsIssue1.equals(layoutReportsIssue2));
 	}
@@ -56,7 +65,19 @@ public class LayoutReportsIssueTest {
 	@Test
 	public void testNewLayoutReportsIssue() {
 		LayoutReportsIssue layoutReportsIssue = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
+
+		List<LayoutReportsIssue.Detail> details =
+			layoutReportsIssue.getDetails();
+
+		Assert.assertEquals(details.toString(), 1, details.size());
+
+		LayoutReportsIssue.Detail detail = details.get(0);
+
+		Assert.assertEquals("title", detail.getKey());
+		Assert.assertEquals(100, detail.getTotal());
 
 		Assert.assertEquals(
 			LayoutReportsIssue.Key.SEO, layoutReportsIssue.getKey());
@@ -69,11 +90,30 @@ public class LayoutReportsIssueTest {
 	}
 
 	@Test
+	public void testNotEqualsWithDifferentDetails() {
+		LayoutReportsIssue layoutReportsIssue1 = new LayoutReportsIssue(
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
+		LayoutReportsIssue layoutReportsIssue2 = new LayoutReportsIssue(
+			Arrays.asList(
+				new LayoutReportsIssue.Detail("hreflang", 50),
+				new LayoutReportsIssue.Detail("title", 50)),
+			LayoutReportsIssue.Key.SEO, 100);
+
+		Assert.assertFalse(layoutReportsIssue1.equals(layoutReportsIssue2));
+	}
+
+	@Test
 	public void testNotEqualsWithDifferentKey() {
 		LayoutReportsIssue layoutReportsIssue1 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 		LayoutReportsIssue layoutReportsIssue2 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.ACCESSIBILITY, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.ACCESSIBILITY, 100);
 
 		Assert.assertFalse(layoutReportsIssue1.equals(layoutReportsIssue2));
 	}
@@ -81,9 +121,13 @@ public class LayoutReportsIssueTest {
 	@Test
 	public void testNotEqualsWithDifferentTotal() {
 		LayoutReportsIssue layoutReportsIssue1 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 		LayoutReportsIssue layoutReportsIssue2 = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 200);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 200);
 
 		Assert.assertFalse(layoutReportsIssue1.equals(layoutReportsIssue2));
 	}
@@ -91,24 +135,37 @@ public class LayoutReportsIssueTest {
 	@Test
 	public void testToJSONObject() {
 		LayoutReportsIssue layoutReportsIssue = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 
 		JSONObject jsonObject = layoutReportsIssue.toJSONObject(
 			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE);
 
+		JSONArray detailsJSONArray = jsonObject.getJSONArray("details");
+
+		Assert.assertEquals(1, detailsJSONArray.length());
+
+		JSONObject detailJSONObject = detailsJSONArray.getJSONObject(0);
+
+		Assert.assertEquals("title", detailJSONObject.getString("key"));
+		Assert.assertEquals(100, detailJSONObject.getLong("total"));
+
 		Assert.assertEquals(
 			LayoutReportsIssue.Key.SEO.toString(), jsonObject.getString("key"));
-		Assert.assertEquals(
-			layoutReportsIssue.getTotal(), jsonObject.getLong("total"));
+		Assert.assertEquals(100, jsonObject.getLong("total"));
 	}
 
 	@Test
 	public void testToString() {
 		LayoutReportsIssue layoutReportsIssue = new LayoutReportsIssue(
-			null, LayoutReportsIssue.Key.SEO, 100);
+			Collections.singletonList(
+				new LayoutReportsIssue.Detail("title", 100)),
+			LayoutReportsIssue.Key.SEO, 100);
 
 		Assert.assertEquals(
-			"{\"total\":\"100\",\"key\":\"seo\"}",
+			"{\"total\":\"100\",\"details\":[{\"total\":\"100\"," +
+				"\"key\":\"title\"}],\"key\":\"seo\"}",
 			layoutReportsIssue.toString());
 	}
 
