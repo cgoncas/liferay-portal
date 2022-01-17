58627e92cf984388fc6b3207ec776204cfb43394
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-01-17 12:17:37 +0100

    LPS-145441 New Tracker for the class ContentDashboardItemSearchClassNameMapper

---
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/ContentDashboardItemSearchClassNameMapperTracker.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.web.internal.item;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.reflect.GenericUtil;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+
+import java.util.Optional;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = ContentDashboardItemSearchClassNameMapperTracker.class)
+public class ContentDashboardItemSearchClassNameMapperTracker {
+
+	public String getContentDashboardItemSearchClassName(String className) {
+		return Optional.ofNullable(
+			_serviceTrackerMap.getService(className)
+		).map(
+			ContentDashboardItemSearchClassNameMapper::getSearchClassName
+		).orElse(
+			className
+		);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap =
+			(ServiceTrackerMap)ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, ContentDashboardItemSearchClassNameMapper.class,
+				null,
+				ServiceReferenceMapperFactory.create(
+					bundleContext,
+					(contentDashboardItemSearchClassNameMapper, emitter) ->
+						emitter.emit(
+							GenericUtil.getGenericClassName(
+								contentDashboardItemSearchClassNameMapper))));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	private volatile ServiceTrackerMap
+		<String, ContentDashboardItemSearchClassNameMapper<?>>
+			_serviceTrackerMap;
+
+}
